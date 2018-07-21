25813f592ca856a95e0ede88d95713a6029efb46
nandini25sharma <nandini.1533@gmail.com>
2018-07-22 01:04:18 +0530

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment= require('moment');
+
+//var date = moment();
+//date.add(1, 'year').subtract(7, 'months')
+//console.log(date.format('MMM Do, YYYY'));
+new Date().getTime()
+var someTimestamp= moment().valueOf();
+console.log(someTimestamp)
+
+var createdAt= 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
